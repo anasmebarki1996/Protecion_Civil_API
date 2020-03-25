c9d445c896491b74ddcbc739ae11b0e9610d6f1a
anasmebarki1996 <anasmebarki1996@outlook.fr>
2020-03-25 19:43:23 +0100

    class apiFeatures ajoutée pour sort , pagination , limit ...

--- controllers/interventionController.js
+++ controllers/interventionController.js
@@ -1,9 +1,14 @@
 const Intervention = require("./../models/interventionModel");
 const catchAsync = require('../utils/catchAsync');
 const dateTime = require('../utils/moment').dateTime;
+const APIFeatures = require('../utils/apiFeatures')
 
 exports.getAllInterventions = catchAsync(async (req, res) => {
-    const interventions = await Intervention.find();
+    // EXECUTE QUERY
+    const features = new APIFeatures(Intervention.find(), req.query).filter().sort().limitFields();
+
+    const interventions = await features.query;
+    // SEND RESPONSE
     res.status(200).json({
         status: "success",
         data: {
@@ -11,6 +16,8 @@ exports.getAllInterventions = catchAsync(async (req, res) => {
         }
     })
 });
+
+
 
 exports.addDateTimeDepart = catchAsync(async (req, res) => {
     Intervention.updateOne({
